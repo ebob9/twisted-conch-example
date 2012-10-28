a106005f123adadd8e6057c36f6678aaa3412542
Gosuke Miyashita <gosukenator@gmail.com>
2012-10-28 13:57:02 -0400

    Add password authentication

--- ssh-server.py
+++ ssh-server.py
@@ -4,6 +4,13 @@ from twisted.cred.portal import Portal
 from twisted.conch.ssh.factory import SSHFactory
 from twisted.internet import reactor
 from twisted.conch.ssh.keys import Key
+from twisted.cred.checkers import FilePasswordDB
+from twisted.conch.interfaces import IConchUser
+from twisted.conch.avatar import ConchUser
+
+from twisted.python import log
+import sys
+log.startLogging(sys.stderr)
 
 with open('id_rsa') as privateBlobFile:
     privateBlob = privateBlobFile.read()
@@ -13,10 +20,20 @@ with open('id_rsa.pub') as publicBlobFile:
     publicBlob = publicBlobFile.read()
     publicKey  = Key.fromString(data=publicBlob)
 
+def nothing():
+    pass
+
+class SimpleRealm(object):
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        return IConchUser, ConchUser(), nothing
+
 factory = SSHFactory()
 factory.privateKeys = { 'ssh-rsa': privateKey }
 factory.publicKeys  = { 'ssh-rsa': publicKey  }
-factory.portal = Portal(None)
+
+factory.portal = Portal(SimpleRealm())
+factory.portal.registerChecker(FilePasswordDB("ssh-passwords"))
 
 reactor.listenTCP(2022, factory)
 reactor.run()
+
